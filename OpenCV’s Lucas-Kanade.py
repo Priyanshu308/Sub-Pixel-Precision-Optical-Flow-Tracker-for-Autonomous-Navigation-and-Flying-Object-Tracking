63951d19592063ef030f7c29f06c5333a6e1f330
Priyanshu Bharadwaj <112001444+Priyanshu308@users.noreply.github.com>
2024-10-26 17:39:17 +0530

    Create OpenCV’s Lucas-Kanade.py

---
+++ OpenCV’s Lucas-Kanade.py
@@ -0,0 +1,9 @@
+import cv2
+
+def compute_optical_flow(prev_frame, next_frame):
+    prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    next_gray = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
+    flow = cv2.calcOpticalFlowFarneback(prev_gray, next_gray, None, 
+                                        pyr_scale=0.5, levels=3, winsize=15, 
+                                        iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
+    return flow
